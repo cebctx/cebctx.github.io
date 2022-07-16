9a571e7bb9a29dfd6d6a94bc9acbac4681e383f3
Van Mualcin <94856452+VanMualcinLSS@users.noreply.github.com>
2022-07-16 00:41:56 -0500

    fixed tailwind config

--- src/js/components/nav/NavBar.jsx
+++ src/js/components/nav/NavBar.jsx
@@ -2,7 +2,7 @@ import Menu from './Menu';
 
 const NavBar = ({ title }) => {
   return (
-    <header className='flex pb-3 justify-between items-center mt-5 h-[50px] w-full'>
+    <header className='flex flex-col pb-3 items-center mt-5 h-[50px] w-full'>
       <h1 className='cebc-title text-2xl select-none subpixel-antialiased'>{ title }</h1>
       <Menu />
     </header>
